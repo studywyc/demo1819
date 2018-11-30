4b2d070dab8cc0a5d7bd640e6fe149ab4919329c
studywyc <1551297585@qq.com>
2018-11-30 22:28:31 +0800

    注册页面

---
+++ items/js/login.js
@@ -0,0 +1,119 @@
+//会员名称
+	var flagName = null;
+	$id("uname").onblur = function(){//要求:单词字符组成  6--10位 ，不能用数字开头
+		var reg = /^[a-z]\w{5,9}$/i;
+		var userTxt = uname.value;
+		if( reg.test( userTxt ) ){
+			flagName = true;
+		}else{
+			$("#s1").css("display","block").stop().fadeToggle(3000);
+			$("#s1").html("单词字符组成  6--10位,不能用数字开头");
+			flagName = false;
+		}
+	}
+	
+	//密码：6--18位字符组成       有密码强弱验证 
+	var flagPwd = null;
+	$id("upwd").onblur = function(){
+		var reg = /^.{6,18}$/;
+		var userTxt = upwd.value;
+		if( reg.test( userTxt )){
+			flagPwd = true;
+		}else{
+			$("#s1").css("display","block").stop().fadeToggle(3000);
+			$("#s1").html("亲 6-18位字符组成哦");
+			flagPwd = false;
+		}	
+	}
+	//确认密码和密码相同
+	var flagQpwd = null;
+	$id("qpwd").onblur = function(){
+		var qpwdTxt = qpwd.value;
+		if( upwd.value != qpwd.value ){
+			$("#s1").css("display","block").stop().fadeToggle(3000);
+			$("#s1").html("密码不一致哦!");
+			flagQpwd = false;
+		}else{
+			flagQpwd = true;
+		}
+	}
+	//邮箱： 必须有@符号
+	var flagEmail = null;
+	$id("email").onblur = function(){
+		var reg = /^[1-9]\d{4,11}@[qQ][qQ]\.(com|cn)$/;
+		var emailTxt = email.value;
+		if( reg.test( emailTxt ) ){
+			flagEmail = true;
+		}else{
+			$("#s1").css("display","block").stop().fadeToggle(3000);
+			$("#s1").html("不符合qq邮箱规范哦!");			
+			flagEmail = false;
+		}
+	}
+	
+	//随机验证码        !!!有问题
+	yz.innerHTML = yzm();
+	$("#btnYzm").click(function(){
+		$("#yz").html( yzm() );
+	})
+	var flagYzm = null;
+/*	$("#yzm").blur(function(){
+		if($("#yzm").html(yzm()) ){
+			alert()
+		}else{
+			alert("no")
+		}
+	})*/
+	$id("yzm").onblur = function(){
+		/*var yzmTxt = yzm.value;
+		var yzTxt = yz.value;*/
+		if( yzm.value = yz.innerHTML){
+			flagYzm = true;
+		}else{
+			$("#s1").css("display","block").stop().fadeToggle(3000);
+			$("#s1").html("no");	
+			flagYzm = false;
+		}
+	}
+	
+	Btn.onclick = function(){
+		if( flagName ){
+			//参数提出来  使用字符串模板
+			var data = `status=register&uname=${uname.value}&upwd=${upwd.value}`;
+			//console.log( data)//id的value可变的
+			var ajax = new XMLHttpRequest();
+			ajax.open("GET","../php/login_register.php?"+data);		
+			ajax.send();
+			ajax.onreadystatechange = function(){
+				if( ajax.status == 200 && ajax.readyState == 4 ){					
+					var res = ajax.responseText;
+					if( res == 0 ){
+						alert( "用户名已存在" );
+					}else if( res == 1 ){
+						alert( "注册成功" );
+						location.href="../php/register.html";
+					}else if( res == 2 ){
+						alert("注册失败");
+					}				
+				}
+			}			
+		}
+	}	
+		/*if( flagName){		
+			return true;
+		}else{
+			return false;
+		}*/
+	
+
+	/*btnTi.onclick = function(){
+		if( flagName){		
+			return true;
+		}else{
+			return false;
+		}
+	}*/
+	
+	
+	
+	
---
+++ items/php/login_register.php
@@ -0,0 +1,51 @@
+<?php	
+	header("content-type:text/html;charset=utf-8");
+	$db = mysql_connect("localhost","root","root");
+	mysql_select_db("bd1819",$db);
+	mysql_query("set names utf8");
+	
+	//接口实现登录注册功能
+	$status = $_GET["status"];//参数控制登录还是注册功能
+	$uname = $_GET["uname"];//接受用户名
+	$upwd = $_GET["upwd"];//接受密码
+	
+	//参数如果是登录功能
+	if( $status =="login" ){
+		//登录功能
+		//编写sql语句
+		$sql = "SELECT * FROM `users` WHERE `uname`='$uname'";
+		$res = mysql_query($sql);//执行sql语句
+		
+		$arr = mysql_fetch_array($res);//转成数组;
+		//判断用户名是否存在
+		if( $arr ){//$arr存在的话
+			if( $upwd == $arr["upwd"]){//用户名和密码相等
+				echo 1;//登录成功
+			}else{
+				echo 2;//登录失败
+			}
+		}else{
+			echo 0; //用户名不存在
+		}
+	}else if( $status =="register" ){//判断用户名是否存在
+		//编写sql语句
+		$sql = "SELECT * FROM `users` WHERE `uname`='$uname'";
+		//执行sql语句
+		$ree = mysql_query($sql);
+		
+		$arr = mysql_fetch_array($ree);
+		if( $arr ){//如果数组里有用户名
+			echo 0;//用户名已存在 
+		}	
+	}else if($status =="register"){//如果是注册功能
+		//编写sql语句
+		$sql = "INSERT INTO `users`( `uname`, `upwd`) VALUES ('$uname','$upwd')";
+		//执行sql语句
+		$row = mysql_query($sql);
+		if( $row ){
+			echo 1;//注册成功
+		}else{
+			echo 2;//注册失败
+		}
+	}
+?>
